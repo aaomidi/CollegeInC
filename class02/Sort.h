8ab4d0cee9403fcb27fcea77e3e0e3f8bdb57113
Amir <aa_omidi@yahoo.com>
2014-10-24 17:58:50 +0330

    Comments

--- class02/Sort.h
+++ class02/Sort.h
@@ -4,8 +4,10 @@ void sort() {
     int i = 0;
     printf("Please enter your number.\n");
     int input = -1;
+    // Initialize the values variable.
     int values[1024];
     scanf("%d", &input);
+    // Read input and assign it to the input variable.
     while (input != -1) {
         values[i] = input;
         i++;
@@ -14,6 +16,7 @@ void sort() {
     }
     int a;
     int b;
+    // Check each variable in the array with the variable right by it.
     for (a = i; a >= 0; a--) {
         for (b = 1; b <= i; b++) {
             if (values[b - 1] > values[b]) {
@@ -23,6 +26,7 @@ void sort() {
             }
         }
     }
+    // Print them out.
     for (a = i; a >= 0; a--) {
         printf("%d\n", values[a]);
     }
