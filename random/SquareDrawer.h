0c1d62d9ba9e77f5570b83db8f1685ebd132ca69
Amir <aa_omidi@yahoo.com>
2014-11-11 21:00:02 +0330

    SquareDrawer!

--- random/SquareDrawer.h
+++ random/SquareDrawer.h
@@ -4,10 +4,27 @@ void drawSquare() {
     printf("Please enter the size of the square you want drawn.\n");
     int a, b, size = 0;
     scanf("%d", &size);
+    /*
     for (a = 1; a <= size; a++) {
         for (b = 1; b <= size; b++) {
             printf("*");
         }
         printf("\n");
     }
+    */
+    a = 1;
+    b = 1;
+    LOOP:
+    if (a <= size) {
+        printf("*");
+        a++;
+        if (a > size && b < size) {
+            printf("\n");
+            a = 1;
+            b++;
+        }
+        goto LOOP;
+    }
+
+
 }
