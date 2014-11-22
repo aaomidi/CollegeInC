17bab80e1fd624a269f415ba0643ad5fe780ffdf
Amir <aa_omidi@yahoo.com>
2014-11-22 14:30:28 +0330

    Finished the grade project, this project is problematic with the cygwin C compiler.

--- projects/Grade.h
+++ projects/Grade.h
@@ -13,14 +13,14 @@ void calculateGPA() {
     * 1 takenCredits
     * 2 passedCredits
     * 3 failedCredits
-    * 4 droppedCredits
+    * 4 incompleteCredits
     * 5 preCollegeCredits
-    * 6 incompleteCredits
+    * 6 droppedCredits
     */
-    int information[7] = {0};
+    int information[7] = {0, 0, 0, 0, 0, 0, 0};
     while (result == 1) {
         char grade;
-        printf("Please enter your letter grade.\n");
+        printf("Please enter the letter grade.\n");
         //Read user entry for grade.
         while (!scanf("%s", &grade)) {
             printf("That grade letter was not recognized. Please enter it again.\n");
@@ -28,35 +28,80 @@ void calculateGPA() {
         setbuf(stdin, NULL);
         grade = tolower(grade);
         result = checkEntry(grade);
-        if (result) {
+        if (result == 1) {
             int credits;
             printf("Please enter the number of credits.\n");
-            while (!scanf("%d", &credits)) {
+            temp = scanf("%d", &credits);
+            while (temp < 1) {
                 setbuf(stdin, NULL);
                 printf("Please enter an integer for credits.\n");
+                temp = scanf("%d", &credits);
             }
+            setbuf(stdin, NULL);
             switch (grade) {
                 case 'a':
+                    // An 'A' is considered 4 points.
+                    information[0] += credits * 4;
+                    information[1] += credits;
+                    information[2] += credits;
                     break;
                 case 'b':
+                    // A 'B' is considered 3 points.
+                    information[0] += credits * 3;
+                    information[1] += credits;
+                    information[2] += credits;
+                    break;
+                case 'c':
+                    // A 'C' is considered 3 points.
+                    information[0] += credits * 2;
+                    information[1] += credits;
+                    information[2] += credits;
                     break;
                 case 'd':
+                    // A 'D' is considered 3 points.
+                    information[0] += credits * 1;
+                    information[1] += credits;
+                    information[2] += credits;
                     break;
                 case 'f':
+                    information[1] += credits;
+                    information[3] += credits;
                     break;
                 case 'i':
+                    information[1] += credits;
+                    information[4] += credits;
                     break;
                 case 'p':
+                    information[1] += credits;
+                    information[5] += credits;
                     break;
                 case 'w':
+                    information[1] += credits;
+                    information[6] += credits;
                     break;
                 default:
                     //Do nothing.
                     break;
             }
-        } else if (!result) {
+        } else if (result == 0) {
             printf("That grade letter was not recognized. Please enter it again.\n");
             result = 1;
+        } else if (result == 2) {
+            printf("Thank you for entering the grades.\n");
+            int i;
+            for (i = 0; i < 70; i++) {
+                printf("-");
+            }
+            // GPA = Net score divided by passed+failed Credits
+            float gpa = ((float) information[0] / (float) (information[2] + information[3]));
+            printf("\nThe student took %d credits. From the %d credits, they passed %d of them where %d of them were pre-college credits."
+                            " Also they failed %d of their taken credits.\n"
+                            "Furthermore they had %d incomplete and %d dropped credits.\n"
+                            "Their GPA is %.2f",
+                    information[1], information[1], information[2] + information[5], information[5],
+                    information[3],
+                    information[4], information[6],
+                    gpa);
         }
     }
 }
